e15a9e385e0a0ce06745ff07bbca7a7cdcd8422c
Du0ngVanBa0 <tidumong789@gmail.com>
2023-01-17 22:31:53 +0700

    push bt ss7

---
+++ Module1/ss6_javascript/thuc_hanh/OutputDisplay/index.js
@@ -0,0 +1,12 @@
+function AlertST() {
+    alert('SomeThing');
+}
+function useHTML() {
+    document.getElementById('HTML').innerHTML='Use HTML';
+}
+function useWrite() {
+    document.write('Use document.write');
+}
+function useLog() {
+    console.log('Su dung COnsole Log');
+}
