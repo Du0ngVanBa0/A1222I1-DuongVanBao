65e0eaa7ad2b5d174b0d2f071741185454d76567
Du0ngVanBa0 <tidumong789@gmail.com>
2023-04-14 10:48:14 +0700

    ss8

---
+++ Module2/src/ss10_dsa_danh_sach/thuc_hanh/easy_linkedlist/TestMyLinkedList.java
@@ -0,0 +1,9 @@
+package ss10_dsa_danh_sach.thuc_hanh.easy_linkedlist;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList myll = new MyLinkedList(10);
+        myll.add(0,9);
+        myll.printList();
+    }
+}
