85127ec8dd32fb6f80f526f182d797c837bc7259
Du0ngVanBa0 <tidumong789@gmail.com>
2023-10-23 19:20:12 +0700

    fix fibonacci es6

--- Module5/es6/bai_tap/fibonacci.js
+++ Module5/es6/bai_tap/fibonacci.js
@@ -1,8 +1,17 @@
-let fibonacci = function(n){
-    if (n==1 || n==2){
+let fibonacci = function (n) {
+    if (n == 1|| n == 2) {
         return 1;
     }
-    return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-console.log(fibonacci(6));
+let sumFibonacci = function (n) {
+    let fibonacciArr = [];1
+    for (let i = 1; i <= n; i++) {
+        fibonacciArr.push(fibonacci(i));
+    }
+    console.log(fibonacciArr)
+    return fibonacciArr.reduce((previousValue, currentValue) => previousValue + currentValue);
+}
+
+console.log(sumFibonacci(6));
