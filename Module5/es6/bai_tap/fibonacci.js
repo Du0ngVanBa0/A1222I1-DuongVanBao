c33d1016eb238ed96141d67e626bd4d8179c4495
Du0ngVanBa0 <tidumong789@gmail.com>
2023-10-23 19:21:48 +0700

    compl es6

--- Module5/es6/bai_tap/fibonacci.js
+++ Module5/es6/bai_tap/fibonacci.js
@@ -1,17 +1,20 @@
-let fibonacci = function (n) {
-    if (n == 1|| n == 2) {
+let fibonacci = function(n){
+    if (n==1){
+        return 0;
+    }
+    if  (n==2){
         return 1;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    return fibonacci(n-1) + fibonacci(n-2);
 };
 
-let sumFibonacci = function (n) {
-    let fibonacciArr = [];1
-    for (let i = 1; i <= n; i++) {
+let sumFibonacci = function(n){
+    let fibonacciArr = [];
+    for (let i = 1; i <= n ; i++) {
         fibonacciArr.push(fibonacci(i));
     }
     console.log(fibonacciArr)
     return fibonacciArr.reduce((previousValue, currentValue) => previousValue + currentValue);
 }
-
-console.log(sumFibonacci(6));
+7
+console.log(sumFibonacci());
