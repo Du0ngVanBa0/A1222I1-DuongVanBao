9f03f3372709cef503104b68a52bc750ff99ea05
Du0ngVanBa0 <tidumong789@gmail.com>
2023-10-30 20:51:45 +0700

    push ex4

---
+++ Module5/react_component_state_lifecycle/bai_tap/expand_collapse_component/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ConditionRenderComponent from "./components/ConditionRenderComponent";
+
+function App() {
+    return (
+        <ConditionRenderComponent/>
+    );
+}
+
+export default App;
